7729055676c362ce5f38bb2368f625ae5ac14346
Dylan_R <dylan@rhinox.training>
2022-08-26 13:07:49 +0200

    [ADD] Added a script that allows for skinned mesh saving (used in saving hand positions as meshesh to use later)

---
+++ Assets/Grapple/Scripts/Utils/SkinnedMeshSaver.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+/// <summary>
+/// This is a tool to create baked meshes from skinned meshes to create them into prefabs.
+/// This script is mainly used to create a baked mesh from a hand position to use later and is not intended for actual runtime use
+/// </summary>
+public class SkinnedMeshSaver : MonoBehaviour
+{
+    [SerializeField] private bool _bakeMesh = false;
+    private void Update()
+    {
+        if (_bakeMesh)
+        {
+            var skinnedMeshes = this.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+            foreach (var skin in skinnedMeshes)
+            {
+                Mesh meshToSave = new Mesh();
+                skin.BakeMesh(meshToSave);
+                AssetDatabase.CreateAsset(meshToSave, "Assets/" + skin.gameObject.name + "_BAKED.mesh");
+                Debug.Log("SkinnedMeshSaver::Update() -> Mesh asset saved as: " + "Assets/" + skin.gameObject.name + "_BAKED.mesh");
+            }
+            _bakeMesh = false;
+        }
+    }
+
+}
